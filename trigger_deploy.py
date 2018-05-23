75840100cae951ae9fef7cfde23cf20fc803f4a8
millen1m <maxim.millen@gmail.com>
2018-05-23 15:54:37 +0100

    stopped supporting python3.3.

--- trigger_deploy.py
+++ trigger_deploy.py
@@ -12,7 +12,7 @@ version = about['__version__']
 failures = py.test.cmdline.main()
 if failures == 0:
     subprocess.check_call(["git", "tag", version, "-m", "version %s" % version])
-    subprocess.check_call(["git", "push", "--tags", "origin", "pypi"])
+    subprocess.check_call(["git", "push", "--tags", "origin"])
 
 # git push --tags origin pypi
 # git tag 0.5.2 -m "version 0.5.2"
