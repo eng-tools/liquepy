a8f0f71a9d8f61d69229a05a71f043e4fda2c6fb
millen1m <maxim.millen@gmail.com>
2019-02-27 13:52:07 +1300

    Added CPT to trigger

--- liquepy/trigger/__init__.py
+++ liquepy/trigger/__init__.py
@@ -1,4 +1,4 @@
-from liquepy.trigger.boulanger_and_idriss_2014 import BoulangerIdriss2014, run_bi2014
+from liquepy.trigger.boulanger_and_idriss_2014 import BoulangerIdriss2014, run_bi2014, BoulangerIdriss2014CPT
 from liquepy.trigger.triggering_measures import calculate_lpi, calculate_lsn
 from liquepy.trigger.volumetric_strain import calculate_volumetric_strain
 from liquepy.trigger.shear_strain import calculate_shear_strain
