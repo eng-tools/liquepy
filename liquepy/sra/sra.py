v0.6.4 * Can change empirical fitting parameter 'c0' in calculation of CRR_m7.5 from BI2016 * Added more logic for FLACSoil model to output to fis file * Fixed issue where could not calculate transfer function in site response analysis

liquepy/sra/sra.py
@@ -15,8 +15,8 @@ def calc_pysra_tf(pysra_profile, pysra_freqs=None):
         pysra_freqs = np.logspace(-0.7, 1.5, num=200)
     m = pysra.motion.Motion(freqs=pysra_freqs)
     outputs = pysra.output.OutputCollection(
-        pysra.output.AccelTransferFunctionOutput(pysra_freqs, pysra.output.OutputLocation('outcrop', index=-1),
-                                                 pysra.output.OutputLocation('outcrop', index=0)),
+        [pysra.output.AccelTransferFunctionOutput(pysra_freqs, pysra.output.OutputLocation('outcrop', index=-1),
+                                                 pysra.output.OutputLocation('outcrop', index=0)),]
     )
     calc = pysra.propagation.LinearElasticCalculator()
     calc(m, pysra_profile, pysra_profile.location('outcrop', index=-1))