64dbc6e6d09cd2f9e3135f43cd3a6c3c306a3de2
millen1m <maxim.millen@gmail.com>
2021-04-19 16:49:33 +1200

    version 0.6.20 - fixed test

--- tests/test_num_flac.py
+++ tests/test_num_flac.py
@@ -6,14 +6,12 @@ import sfsimodels as sm
 
 def test_flac_set_g0_mod():
     sl = lq.num.flac.PM4Sand()
-    sl.phi = 30.
+    # sl.phi = 30.
+    sl.poissons_ratio = 0.3
     sl.set_g0_mod_from_g_mod_at_v_eff_stress(20.0e6, 50.0e3)
     assert np.isclose(sl.get_g_mod_at_v_eff_stress(50.0e3), 20.0e6)
     assert np.isclose(sl.get_g_mod_at_v_eff_stress(100.0e3), 28284271.24746)
-    assert np.isclose(sl.get_g_mod_at_m_eff_stress(50.0e3), 23094010.767585)
-    k0 = 1 - np.sin(sl.phi_r)
-    m_eff = (1 + k0) / 2 * 50.0e3
-    assert np.isclose(sl.get_g_mod_at_m_eff_stress(m_eff), 20.0e6)
+    assert np.isclose(sl.get_g_mod_at_m_eff_stress(50.0e3), 23664319.132398)
 
 
 def test_write_obj_to_fis_str():
